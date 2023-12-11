2fbe29b90485eca0035becd8d84dcb6b789a377f
j178 <10510431+j178@users.noreply.github.com>
2023-12-11 14:47:15 +0800

    add maps, slices

---
+++ itmaps/maps.go
@@ -0,0 +1,52 @@
+package itmaps
+
+import "iter"
+
+// All returns an iterator over key-value pairs from m.
+func All[Map ~map[K]V, K comparable, V any](m Map) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range m {
+			if !yield(k, v) {
+				break
+			}
+		}
+	}
+}
+
+// Keys returns an iterator over keys in m.
+func Keys[Map ~map[K]V, K comparable, V any](m Map) iter.Seq[K] {
+	return func(yield func(K) bool) {
+		for k := range m {
+			if !yield(k) {
+				break
+			}
+		}
+	}
+}
+
+// Values returns an iterator over values in m.
+func Values[Map ~map[K]V, K comparable, V any](m Map) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for _, v := range m {
+			if !yield(v) {
+				break
+			}
+		}
+	}
+}
+
+// Insert adds the key-value pairs from seq to m.
+func Insert[Map ~map[K]V, K comparable, V any](m Map, seq iter.Seq2[K, V]) Map {
+	for k, v := range seq {
+		m[k] = v
+	}
+	return m
+}
+
+// Collect collects key-value pairs from seq into a new map
+// and returns it.
+func Collect[K comparable, V any](seq iter.Seq2[K, V]) map[K]V {
+	m := make(map[K]V)
+	Insert(m, seq)
+	return m
+}
---
+++ itslices/slice.go
@@ -0,0 +1,72 @@
+//go:build goexperiment.rangefunc
+
+package itslices
+
+import (
+	"cmp"
+	"iter"
+	"slices"
+)
+
+// All returns an iterator over index-value pairs in the slice.
+// The indexes range in the usual order, from 0 through len(s)-1.
+func All[Slice ~[]Elem, Elem any](s Slice) iter.Seq2[int, Elem] {
+	return func(yield func(int, Elem) bool) {
+		for i, v := range s {
+			if !yield(i, v) {
+				break
+			}
+		}
+	}
+}
+
+// Backward returns an iterator over index-value pairs in the slice,
+// traversing it backward. The indexes range from len(s)-1 down to 0.
+func Backward[Slice ~[]Elem, Elem any](s Slice) iter.Seq2[int, Elem] {
+	return func(yield func(int, Elem) bool) {
+		for i := len(s) - 1; i >= 0; i-- {
+			if !yield(i, s[i]) {
+				break
+			}
+		}
+	}
+}
+
+// Values returns an iterator over the values in the slice,
+// starting with s[0].
+func Values[Slice ~[]Elem, Elem any](s Slice) iter.Seq[Elem] {
+	return func(yield func(Elem) bool) {
+		for _, v := range s {
+			if !yield(v) {
+				break
+			}
+		}
+	}
+}
+
+// Append appends the values from seq to the slice and returns the extended slice.
+func Append[Slice ~[]Elem, Elem any](x Slice, seq iter.Seq[Elem]) Slice {
+	for _, v := range seq {
+		x = append(x, v)
+	}
+	return x
+}
+
+// Collect collects values from seq into a new slice and returns it.
+func Collect[Elem any](seq iter.Seq[Elem]) []Elem {
+	return Append([]Elem(nil), seq)
+}
+
+// Sorted collects values from seq into a new slice, sorts the slice, and returns it.
+func Sorted[Elem cmp.Ordered](seq iter.Seq[Elem]) []Elem {
+	slice := Collect(seq)
+	slices.Sort(slice)
+	return slice
+}
+
+// SortedFunc collects values from seq into a new slice, sorts the slice, and returns it.
+func SortedFunc[Elem any](seq iter.Seq[Elem], cmp func(Elem, Elem) int) []Elem {
+	slice := Collect(seq)
+	slices.SortFunc(slice, cmp)
+	return slice
+}
